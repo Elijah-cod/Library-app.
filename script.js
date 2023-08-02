254f80cd63ab3b53b8d33c0e91f75fca96b1690c
Elijah <iamngunjiri@gmail.com>
2023-08-02 10:18:08 +0300

    Setting up the skeleton for the project

---
+++ script.js
@@ -0,0 +1,7 @@
+// Function to display the alert message
+function showAlert() {
+    alert("The page is clicked!");
+  }
+
+  // Add click event listener to the whole document
+  document.addEventListener("click", showAlert);
